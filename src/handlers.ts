e083f9c58d3dd2616d6b8e65a19e4c82d6a0fce3
daniel-budai <danielbudai@tutanota.com>
2024-09-02 19:34:05 +0200

    node 18, getproducts

--- src/handlers.ts
+++ src/handlers.ts
@@ -7,16 +7,33 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 export const createProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const reqBody = JSON.parse(event.body as string);
   const product = { ...reqBody, productID: v4() };
+  const tableName = "ProductsTable";
 
   await docClient
     .put({
-      TableName: "ProductsTable",
+      TableName: tableName,
       Item: product,
     })
     .promise();
 
   return {
-    statusCode: 200,
+    statusCode: 201, // 201 Created
     body: JSON.stringify(product),
+  };
+};
+
+export const getProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const id = event.pathParameters?.id; //? because id may be undefined
+
+  const output = await docClient.get({ TableName: "tableName", Key: { productID: id } }).promise();
+  if (!output.Item) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ message: "Product not found" }),
+    };
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(output.Item),
   };
 };
